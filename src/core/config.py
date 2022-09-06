db80adb9092c434f94fe19dd4b198207477c170c
kenki <ilsafganiev2222@gmail.com>
2022-09-06 17:47:16 +0300

    project structure

---
+++ src/core/config.py
@@ -0,0 +1,50 @@
+from logging import config as logging_config
+from pathlib import Path
+
+from pydantic import BaseSettings
+
+from core.logger import LOGGING
+
+logging_config.dictConfig(LOGGING)
+
+
+class BaseConfig(BaseSettings):
+    class Config:
+        env_file = Path(Path(__file__).parent.parent.parent, '.env')
+        env_file_encoding = 'utf-8'
+
+
+class RedisSettings(BaseConfig):
+    HOST: str = '127.0.0.1'
+    PORT: int = 6379
+
+    class Config:
+        env_prefix = 'REDIS_'
+
+    @property
+    def url(self):
+        return f'redis://{self.HOST}:{self.PORT}'
+
+
+class PostgresSettings(BaseConfig):
+    USER: str
+    PASSWORD: str
+    DB: str
+    HOST: str = '127.0.0.1'
+    PORT: int = 5432
+
+    @property
+    def uri(self):
+        return f'postgresql://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DB}'
+
+    class Config:
+        env_prefix = 'PG_'
+
+
+class ProjectSettings(BaseConfig):
+
+    redis: RedisSettings = RedisSettings()
+    postgres: PostgresSettings = PostgresSettings()
+
+
+settings = ProjectSettings()
