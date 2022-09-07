212da75f35106552ad9c0b9c1afb83337ceae9f8
Galovach Andrey <a.galovach@yandex.ru>
2022-09-07 18:16:26 +0300

    Добавил roles в модель User

--- src/models/user.py
+++ src/models/user.py
@@ -1,19 +1,23 @@
 from datetime import datetime
-from db.db import db
-from utils import BaseModel
+
 from flask_security import UserMixin
+
+from db.db import db
+from models.utils import BaseModel
 
 
 class User(BaseModel, UserMixin):
     __tablename__ = 'users'
-
     username = db.Column(db.String(length=150), unique=True, nullable=False, index=True)
     password = db.Column(db.String(length=150), nullable=False)
     email = db.Column(db.String(length=150), unique=True, nullable=False, index=True)
     user_agent = db.Column(db.String, nullable=False)
     active = db.Column(db.Boolean(), nullable=False)
+    roles = db.Column(db.ARRAY(db.String), nullable=False)
     create_at = db.Column(db.TIMESTAMP, default=datetime.now(), nullable=False)
-    updated_at = db.Column(db.TIMESTAMP, default=datetime.now(), onupdate=datetime.now(), nullable=False)
+    updated_at = db.Column(
+        db.TIMESTAMP, default=datetime.now(), onupdate=datetime.now(), nullable=False
+    )
 
     def __repr__(self) -> str:
         return f'User: {self.username} {self.id}'
