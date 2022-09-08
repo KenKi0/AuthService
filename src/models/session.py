d226db413163359441b1d92bd7d5ff7c215382c8
Galovach Andrey <a.galovach@yandex.ru>
2022-09-08 17:10:41 +0300

    Мелкие исправления UUID

--- src/models/session.py
+++ src/models/session.py
@@ -1,12 +1,14 @@
 from datetime import datetime
 
+from sqlalchemy.dialects.postgresql import UUID
+
 from db.db import db
 from models.utils import BaseModel
 
 
 class Session(BaseModel):
     __tablename__ = 'sessions'
-    user_id = db.Column(db.String, db.ForeignKey('auth.users.id'), nullable=False)
+    user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('auth.users.id'), nullable=False)
     refresh_token = db.Column(db.String, nullable=False)
     updated_at = db.Column(db.TIMESTAMP, default=datetime.now(), onupdate=datetime.now(), nullable=False)
 
@@ -16,7 +18,7 @@ class Session(BaseModel):
 
 class LoginInfo(BaseModel):
     __tablename__ = 'login_info'
-    user_id = db.Column(db.String, db.ForeignKey('auth.users.id'), nullable=False)
+    user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('auth.users.id'), nullable=False)
     user_agent = db.Column(db.String, nullable=False)
     date = db.Column(db.TIMESTAMP, default=datetime.now(), nullable=False)
 
