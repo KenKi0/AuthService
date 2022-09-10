33acd2d4138a5e119e634a79a955afbe8a23263a
Galovach Andrey <a.galovach@yandex.ru>
2022-09-10 14:36:56 +0300

    Исправил models (убрал связи из моделей)

--- src/models/role.py
+++ src/models/role.py
@@ -10,19 +10,6 @@ class Role(BaseModel, RoleMixin):
     name = db.Column(db.String(length=150), unique=True, nullable=False, index=True)
     description = db.Column(db.String(length=150), nullable=False)
 
-    users = db.relationship(
-        'User',
-        secondary='auth.users_roles',
-        lazy='subquery',
-        back_populates='roles',
-    )
-    permissions = db.relationship(
-        'Permission',
-        secondary='auth.roles_permissions',
-        lazy='subquery',
-        back_populates='roles',
-    )
-
     def __repr__(self) -> str:
         return f'Role: {self.name} {self.id}'
 
