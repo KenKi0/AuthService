803537dd8f7e0607c103d2b29954a4b6c7a2665c
Galovach Andrey <a.galovach@yandex.ru>
2022-09-10 06:07:02 +0300

    Исправил users

--- src/models/user.py
+++ src/models/user.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 
-from flask_security import UserMixin
-from sqlalchemy import String
+from flask_security import SQLAlchemyUserDatastore, UserMixin
+from sqlalchemy.dialects.postgresql import UUID
 
 from db.db import db
+from models.role import Role
 from models.utils import BaseModel
 
 
@@ -12,12 +13,25 @@ class User(BaseModel, UserMixin):
     username = db.Column(db.String(length=150), unique=True, nullable=False, index=True)
     password = db.Column(db.String(length=150), nullable=False)
     email = db.Column(db.String(length=150), unique=True, nullable=False, index=True)
-    user_agent = db.Column(db.String, nullable=False)
     active = db.Column(db.Boolean(), nullable=False)
-    roles = db.Column(db.ARRAY(String), nullable=False)
+    roles = db.relationship('Role', secondary='auth.users_roles', lazy='subquery', back_populates='users')
     is_super = db.Column(db.Boolean(), default=False)
-    create_at = db.Column(db.TIMESTAMP, default=datetime.now(), nullable=False)
-    updated_at = db.Column(db.TIMESTAMP, default=datetime.now(), onupdate=datetime.now(), nullable=False)
+    salt = db.Column(db.String(length=150), unique=True, nullable=False, index=True)
 
     def __repr__(self) -> str:
         return f'User: {self.username} {self.id}'
+
+
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+
+
+class UserInfo(BaseModel):
+    __tablename__ = 'user_info'
+    user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('auth.users.id'), nullable=False)
+    full_name = db.Column(db.String, nullable=False)
+    country = db.Column(db.String, nullable=False)
+    gender = db.Column(db.String, nullable=False)
+    birthday = db.Column(db.TIMESTAMP, default=datetime.now(), nullable=False)
+
+    def __repr__(self) -> str:
+        return f'Login: {self.id} {self.date}'
