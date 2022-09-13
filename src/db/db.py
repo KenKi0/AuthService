1f5597b18c4c4b0101a0d74a91ffb029fefdc0a4
Galovach Andrey <a.galovach@yandex.ru>
2022-09-13 04:47:20 +0300

    Добавил создание таблиц

--- src/db/db.py
+++ src/db/db.py
@@ -6,12 +6,13 @@ from core.config import settings
 db = SQLAlchemy()
 
 
-def init_db(app: Flask) -> None:
+def init_db(app: Flask, config: str = settings.postgres.uri) -> None:
     """
     Подключение к БД.
     :param app: Flask
+    :param config: settings
     """
-    app.config['SECRET_KEY'] = settings.SECRET
-    app.config['SQLALCHEMY_DATABASE_URI'] = settings.postgres.uri()
-    app.config['SECRET_PASSWORD_SALT'] = settings.PASSWORD_SALT
+    app.config['SQLALCHEMY_DATABASE_URI'] = config
     db.init_app(app)
+    app.app_context().push()
+    db.create_all()
