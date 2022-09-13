a5955389cc218846536614cadda2991b5c34cc8b
Galovach Andrey <a.galovach@yandex.ru>
2022-09-13 04:43:35 +0300

    Добавил настройки Flask и JWT

--- src/core/config.py
+++ src/core/config.py
@@ -42,14 +42,29 @@ class PostgresSettings(BaseConfig):
         env_prefix = 'PG_'
 
 
-class ProjectSettings(BaseConfig):
+class FlaskSettings(BaseConfig):
+    HOST: str = '127.0.0.1'
+    PORT: int = 5000
 
+    class Config:
+        env_prefix = 'FLASK_'
+
+
+class JWTSettings(BaseConfig):
     SECRET: str = '245585dbb5cbe2f151742298d61d364880575bff0bdcbf4ae383f0180e7e47dd'
+    REFRESH_TOKEN_EXP: timedelta = timedelta(days=10)
+    ACCESS_TOKEN_EXP: timedelta = timedelta(minutes=20)
+
+    class Config:
+        env_prefix = 'JWT_'
+
+
+class ProjectSettings(BaseConfig):
     PASSWORD_SALT: str = 'a5a8f573-3cee-4ccc-8a2b-91cb9f55250a'
-    REFRESH_TOKEN_EXP_DELTA: timedelta = timedelta(days=10)
-    ACCESS_TOKEN_EXP_DELTA: timedelta = timedelta(minutes=20)
     redis: RedisSettings = RedisSettings()
     postgres: PostgresSettings = PostgresSettings()
+    flask: FlaskSettings = FlaskSettings()
+    jwt: JWTSettings = JWTSettings()
 
 
 settings = ProjectSettings()
