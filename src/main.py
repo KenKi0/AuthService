c58276048ab339198635f2a027f3c08054b3eb63
Galovach Andrey <a.galovach@yandex.ru>
2022-09-14 06:51:33 +0300

    Добавил init_security, init_api

--- src/main.py
+++ src/main.py
@@ -1,17 +1,15 @@
-from flasgger import Swagger
 from flask import Flask
 from flask_jwt_extended import JWTManager
+from flask_security import Security
 
-from api.v1.user import auth_blueprint
+from api.v1.user import api, auth_blueprint
 from core.config import settings
 from db.db import init_db
 
 jwt = JWTManager()
 app = Flask(__name__)
+security = Security()
 
-app.config['SECRET_PASSWORD_SALT'] = settings.PASSWORD_SALT
-
-swagger = Swagger(app, config=settings.SWAGGER_CONFIG)
 app.register_blueprint(auth_blueprint)
 
 
@@ -20,9 +18,20 @@ def init_jwt(app: Flask, config: object = settings.jwt) -> None:
     jwt.init_app(app)
 
 
+def init_api(app: Flask) -> None:
+    api.register(app)
+
+
+def init_security(app: Flask, config: object = settings.security) -> None:
+    app.config.from_object(config)
+    security.init_app(app)
+
+
 def main():
     init_db(app)
     init_jwt(app)
+    init_api(app)
+    init_security(app)
     app.run(debug=True, host='0.0.0.0', port=5000)
 
 
