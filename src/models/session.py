f4fb3d18846ea4ac4454fe62cbb229d9021490ff
Galovach Andrey <a.galovach@yandex.ru>
2022-09-15 03:42:10 +0300

    Добавил индексы

--- src/models/session.py
+++ src/models/session.py
@@ -6,6 +6,16 @@ from db.db import db
 from models.utils import BaseModel
 
 
+class AllowedDevice(BaseModel):
+    __tablename__ = 'allowed_device'
+    __table_args__ = (db.UniqueConstraint('user_id', 'user_agent'), {'schema': 'auth'})
+    user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('auth.users.id'), nullable=False)
+    user_agent = db.Column(db.String, nullable=False)
+
+    def __repr__(self) -> str:
+        return f'Device: {self.id}'
+
+
 class Session(BaseModel):
     __tablename__ = 'sessions'
     user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('auth.users.id'), nullable=False)
@@ -14,12 +24,3 @@ class Session(BaseModel):
 
     def __repr__(self) -> str:
         return f'Session: {self.id} {self.auth_date}'
-
-
-class AllowedDevice(BaseModel):
-    __tablename__ = 'allowed_device'
-    user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('auth.users.id'), nullable=False)
-    user_agent = db.Column(db.String, nullable=False)
-
-    def __repr__(self) -> str:
-        return f'Device: {self.id}'
