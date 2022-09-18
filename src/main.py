3ed796c33a9c3cf90f7b67b486e39a568edfb6d8
Galovach Andrey <a.galovach@yandex.ru>
2022-09-18 14:00:50 +0300

    Реализовал создание superuser через консоль

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 import json
 from pathlib import Path
 
+import click
 from apispec import APISpec
 from apispec.ext.marshmallow import MarshmallowPlugin
 from apispec_webframeworks.flask import FlaskPlugin
 from flask import Flask, send_from_directory
+from flask.cli import with_appcontext
 from flask_jwt_extended import JWTManager
 from flask_security import Security
 from flask_swagger_ui import get_swaggerui_blueprint
@@ -16,13 +18,13 @@ from api.v1.permission import permissions_blueprint
 from api.v1.role import role_blueprint
 from api.v1.user import auth_blueprint, user_blueprint
 from core.config import settings
-from db.db import init_db
+from db.db import db, init_db
 from models.permissions import create_permission
 from models.role import create_role
-from models.user import create_super
+from models.user import User
 
 jwt = JWTManager()
-app = Flask(__name__)
+
 security = Security()
 swagger_ui = get_swaggerui_blueprint(
     settings.swagger.SWAGGER_URL,
@@ -30,11 +32,13 @@ swagger_ui = get_swaggerui_blueprint(
     config={'app_name': 'My App'},
 )
 
-app.register_blueprint(swagger_ui, url_prefix=settings.swagger.SWAGGER_URL)
-app.register_blueprint(auth_blueprint)
-app.register_blueprint(user_blueprint)
-app.register_blueprint(role_blueprint)
-app.register_blueprint(permissions_blueprint)
+
+def init_blueprint(app: Flask):
+    app.register_blueprint(swagger_ui, url_prefix=settings.swagger.SWAGGER_URL)
+    app.register_blueprint(auth_blueprint)
+    app.register_blueprint(user_blueprint)
+    app.register_blueprint(role_blueprint)
+    app.register_blueprint(permissions_blueprint)
 
 
 def init_jwt(app: Flask, config: object = settings.jwt) -> None:
@@ -89,17 +93,46 @@ def init_spec(app: Flask) -> None:
         return send_from_directory('static', path)
 
 
-def main(app: Flask = app):
+def init_cli(app: Flask):
+    @with_appcontext
+    @app.cli.command('create_sudo')
+    @click.argument('name')
+    @click.argument('mail')
+    @click.argument('password')
+    def create_sudo(name: str, mail: str, password: str):
+        _admin = {
+            'username': name,
+            'password': mail,
+            'email': password,
+            'is_super': True,
+        }
+        admin = User.query.filter_by(email=_admin['email']).first()
+        if admin:
+            return
+        admin = User(**_admin)
+        db.session.add(admin)
+        db.session.commit()
+
+
+def create_app():
+
+    app = Flask(__name__)
+
+    init_blueprint(app)
     init_db(app)
     init_jwt(app)
     init_security(app)
     init_spec(app)
-    create_super()  # ЗАГЛУШКА! (пока не реализован метод добавления через терминал)
+    init_cli(app)
+
+    return app
+
+
+def main(app: Flask):
     create_permission()
     create_role()
-
     app.run(debug=True, host='0.0.0.0', port=5000)
 
 
 if __name__ == '__main__':
-    main(app)
+    main(create_app())
--- src/wsgi.py
+++ src/wsgi.py
@@ -5,8 +5,7 @@ monkey.patch_all()
 from gevent.pywsgi import WSGIServer  # noqa: E402
 
 from core.config import settings  # noqa: E402
-from main import app, main  # noqa: E402
+from main import create_app, main  # noqa: E402
 
-app_port = settings.flask.PORT
-server = WSGIServer(('0.0.0.0', app_port), main(app))
+server = WSGIServer(('0.0.0.0', settings.flask.PORT), main(create_app()))
 server.serve_forever()
