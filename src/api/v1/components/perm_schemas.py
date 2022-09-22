3c120564b3f85069c33f938e094e73a9503c64d0
Galovach Andrey <a.galovach@yandex.ru>
2022-09-22 06:50:49 +0300

    убрал часть warnings в тестах

--- src/api/v1/components/perm_schemas.py
+++ src/api/v1/components/perm_schemas.py
@@ -2,6 +2,8 @@ from marshmallow import Schema, fields
 
 
 class Permission(Schema):
-    name = fields.Str(description='name', required=True, example='Default user')
-    code = fields.Integer(description='code', required=True, example=0)
-    description = fields.Str(description='description', required=True, example='Может просматривать свой контент')
+    name = fields.Str(metadata={'description': 'name', 'required': True, 'example': 'Default user'})
+    code = fields.Integer(metadata={'description': 'code', 'required': True, 'example': 0})
+    description = fields.Str(
+        metadata={'description': 'description', 'required': True, 'example': 'Может просматривать свой контент'},
+    )
--- src/api/v1/components/role_schemas.py
+++ src/api/v1/components/role_schemas.py
@@ -2,5 +2,5 @@ from marshmallow import Schema, fields
 
 
 class Role(Schema):
-    name = fields.Str(description='name', required=True, example='test_role')
-    description = fields.Str(description='description', required=True, example='Test')
+    name = fields.Str(metadata={'description': 'name', 'required': True, 'example': 'test_role'})
+    description = fields.Str(metadata={'description': 'description', 'required': True, 'example': 'Test'})
